944c677d3d73b8e75a0b2c7bc981f38e6b9b0bf0
Florian Rüberg <florian.rueberg@gmail.com>
2018-05-31 17:37:32 +0200

    Dynamic import start

--- packages/anyui-react/src/import.js
+++ packages/anyui-react/src/import.js
@@ -1,21 +1,41 @@
 
-export function retrieveNpm(name, version) {
-    var request = new XMLHttpRequest();
-//request.open('GET', 'https://unpkg.com/react@latest/dist/react.min.js', false);
-//request.open('GET', 'https://www.npmjs.com/search/suggestions?q=node', false);
-
-request.open('GET', 'https://unpkg.com/react@15.4.1/dist/react.min.js', false);
-
-try {
-	request.send(null);
-  if (request.status === 200) {
-    console.log(request.responseText);
-  }
-} catch(err) {
-	console.log(err);
+export function retrieveNpm(name, version, path) {
+    return fetch(`https://unpkg.com/${name}@${version}/${path}`).then(r => r.text());
 }
+
+function loadScript(code){
+    return new Promise(function(resolve, reject) {
+        var el = document.createElement('script');
+        el.appendChild(document.createTextNode(code));
+        el.onload = resolve;
+        document.head.appendChild(el);
+    });
 }
 
 export function dynamicImport(code) {
-    return ''
+    /*
+    (function () {
+        var define, exports = {};
+        if (window.define && window.define.amd) {
+            define = window.define;
+        } else {
+            exports = window;
+            define = function (name, dependencies, fn) {
+                var deps = [];
+                for (var i = 0; i < dependencies.length; i++)
+                    deps.push(window[dependencies[i]]);
+                var module = fn.apply(undefined, deps);
+                if (!window[name]) window[name] = module;
+            };
+        }
+
+        define('mylib.interaction', ['jQuery', 'mylib.core', 'jQuery.UI'], function($, mylib) {
+            return ;
+        })
+    })()
+    */
+
+    // TODO dont expose REACT to window
+    return loadScript(code).then(() => window.React);
 }
+
--- packages/anyui-react/src/import.test.js
+++ packages/anyui-react/src/import.test.js
@@ -1,7 +1,7 @@
 import {retrieveNpm, dynamicImport} from './import';
 
 describe('dynamic imports', () => {
-	test.skip('Should work for umd type modules', done => {
+	it('Should work for umd type modules', done => {
         retrieveNpm('react', '15.4.1', 'dist/react.min.js')
             .then(dynamicImport)
             .then((ReactImported) => {
