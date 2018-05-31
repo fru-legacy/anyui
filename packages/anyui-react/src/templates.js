5fcca40a61a072dac92797c7e9b88ffb27700679
Florian Rüberg <florian.rueberg@gmail.com>
2018-06-01 00:08:18 +0200

    Cleanup variables

--- packages/anyui-react/src/templates.js
+++ packages/anyui-react/src/templates.js
@@ -1,14 +1,14 @@
 import {parseText, parseCode} from './transpile';
 
-function renderChildren(React, scope, children) {
+function renderChildren(React, variables, children) {
     if (!children) return (x) => []; 
-    var renderers = children.map(c => renderJson(React, scope, c));
+    var renderers = children.map(c => renderJson(React, variables, c));
     return (x) => renderers.map(r => r(x));
 }
 
-function renderTagName(scope, name) {
+function renderTagName(variables, name) {
     if (name[0] === name[0].toUpperCase()) {
-        return parseCode(scope, name);
+        return parseCode(variables, name);
     } else {
         return (x) => name;
     }
@@ -27,24 +27,20 @@ function getPropsByPrefix(prefix, object) {
     return results;
 }
 
-function extendAdded(scope, added, key) {
-
-    return added;
-}
-
 // Attributes can extend the scope, change definitions and influence child rendering
 // This considers the attributes:
 // if="exp"
 // repeat="exp" 
 // repeat-item="name"
 // define-{name}="exp"
-function renderAttributes(scope, attributes) {
-    var added = [];
+function renderAttributes(variables, attributes) {
+
     var scopeTransforms = [];
 
     for(var define of getPropsByPrefix('define-', attributes)) {
-        let transform = parseCode(scope, define.value);
-        added = extendAdded(scope, added, define.key);
+        let transform = parseCode(variables, define.value);
+
+        added = extendAdded(variables, added, define.key);
         scopeTransforms.push((x) => x[define.key] = transform(x))
     }
 
@@ -60,26 +56,25 @@ function renderAttributes(scope, attributes) {
         }
     }
 
-    return {added, scopeTransform, childrenTransform};
+    return {extendedVars: [], scopeTransform, childrenTransform};
 }
 
 // Renders properties considering the following attributes:
 // props="exp"
 // prop-{name}="exp"
 // All other static attributes
-function renderProps(scope, attributes) {
+function renderProps(variables, attributes) {
     return (x) => {};
 }
 
-export function renderJson(React, scope, json) {
+export function renderJson(React, variables, json) {
     if (json.type === 'element') {
 
-        let tag = renderTagName(scope, json.name);
-        let {added, scopeTransform, childrenTransform} = renderAttributes(scope, json.attributes);
-        var extendedScope = scope.concat(added);
+        let tag = renderTagName(variables, json.name);
+        let {extendedVars, scopeTransform, childrenTransform} = renderAttributes(variables, json.attributes);
 
-        let props = renderProps(extendedScope, json.attributes);
-        let children = renderChildren(React, extendedScope, json.children);
+        let props = renderProps(extendedVars, json.attributes);
+        let children = renderChildren(React, extendedVars, json.children);
 
         return (x) => {
             var xdash = scopeTransform(x);
@@ -87,6 +82,6 @@ export function renderJson(React, scope, json) {
             return React.createElement(tag(x), props(xdash), ...cdash);
         };
     } else if (json.type === 'text') {
-        return parseText(scope, json.value);
+        return parseText(variables, json.value);
     }
 }
